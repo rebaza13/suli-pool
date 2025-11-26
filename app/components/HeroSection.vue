f36adab1215fce2d2f71add2f724bd15bdb20244
rebaza13 <rebazking13v@gmail.com>
2025-11-26 12:25:48 +0300

    Refactor code structure for improved readability and maintainability

---
+++ app/components/HeroSection.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="page">
+        <h1>Welcome to the hero Page</h1>
+        <p>This is the main landing page of the application.</p>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: [
+  // SCSS file in the project
+  "~/assets/scss/main.scss", 
+],
+
+  modules: ["@nuxtjs/i18n"],
 })
